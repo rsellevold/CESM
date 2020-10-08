919f99dec579f6ee2a5df2ddcea41a33dc830aee
rsellevold <raymond.sellevold@gmail.com>
2020-10-08 19:14:29 +0200

    First commit of documentations

--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-# Processing modules
-from .mpimods import *
-from .preproc import * # Merging and averaging of history files
-from .proc import * # Basic processing functions
--- new_case.py
+++
@@ -1,9 +0,0 @@
-import sys,os
-
-args = sys.argv
-
-new_folder = sys.argv[1]
-
-os.system(f"mkdir -p {new_folder}")
-os.system(f"cp config.yml {new_folder}/.")
-os.system(f"cp main_proc.py {new_folder}/.")
