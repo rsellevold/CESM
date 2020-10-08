bbee26d594d275f2228ebea1e03366e850f05e71
rsellevold <raymond.sellevold@gmail.com>
2020-10-08 14:14:25 +0200

    Support for <<external>> cases

---
+++ new_case.py
@@ -0,0 +1,9 @@
+import sys,os
+
+args = sys.argv
+
+new_folder = sys.argv[1]
+
+os.system(f"mkdir -p {new_folder}")
+os.system(f"cp config.yml {new_folder}/.")
+os.system(f"cp main_proc_mpi {new_folder}/.")
