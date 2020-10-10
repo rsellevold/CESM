4ed542356eee8885d38f9b46079f394dee091b83
rsellevold <raymond.sellevold@gmail.com>
2020-10-10 10:44:57 -0600

    Calcadd for lnd

--- process/lnd/calcadd.py
+++ process/lnd/calcadd.py
@@ -12,66 +12,118 @@ import lib
 def calcadd(fdir, var):
     if var=="ALBEDO":
         try:
-            FSNS = xr.open_dataset(f"{fdir}/FSNS.nc")
-            FSDS = xr.open_dataset(f"{fdir}/FSDS.nc")
-            ALBEDO = 1 - (FSNS["FSNS"]/FSDS["FSDS"])
-            ALBEDO.name = "ALBEDO"
-            ALBEDO.to_dataset()
-            ALBEDO.encoding["unlimited_dims"] = "time"
-            ALBEDO.to_netcdf(f"{fdir}/ALBEDO.nc")
-            ALBEDO.close()
+            ds = xr.open_mfdataset([f"{fdir}/FSR_ICE.nc",f"{fdir}/FSDS.nc"], combine="nested")
+            ALBEDO_ICE = ds["FSR_ICE"]/ds["FSDS"]
+            ALBEDO_ICE.name = "ALBEDO_ICE"
+            ALBEDO_ICE = ALBEDO_ICE.to_dataset()
+            ALBEDO_ICE.encoding["unlimited_dims"] = "time"
+            ALBEDO_ICE.to_netcdf(f"{fdir}/ALBEDO_ICE.nc")
+            ALBEDO_ICE.close()
+            ds.close()
         except:
             None
 
-    elif var=="PRECT":
+    if var=="MELT_ICE":
         try:
-            PRECC = xr.open_dataset(f"{fdir}/PRECC.nc")
-            PRECL = xr.open_dataset(f"{fdir}/PRECL.nc")
-            PRECT = PRECC["PRECC"] + PRECL["PRECL"]
-            PRECT.name = "PRECT"
-            PRECT.to_dataset()
-            PRECT.encoding["unlimited_dims"] = "time"
-            PRECT.to_netcdf(f"{fdir}/PRECT.nc")
-            PRECT.close()
+            ds = xr.open_mfdataset([f"{fdir}/QSNOMELT_ICE.nc", f"{fdir}/QICE_MELT.nc"], combine="nested")
+            MELT_ICE = ds["QSNOMELT_ICE"] + ds["QICE_MELT"]
+            MELT_ICE.name = "MELT_ICE"
+            MELT_ICE = MELT_ICE.to_dataset()
+            MELT_ICE.encoding["unlimited_dims"] = "time"
+            MELT_ICE.to_netcdf(f"{fdir}/MELT_ICE.nc")
+            MELT_ICE.close()
+            ds.close()
         except:
             None
 
-    elif var=="SNOW":
+    if var=="PRECIP_ICE":
         try:
-            PRECSC = xr.open_dataset(f"{fdir}/PRECSC.nc")
-            PRECSL = xr.open_dataset(f"{fdir}/PRECSL.nc")
-            SNOW = PRECSC["PRECSC"] + PRECSL["PRECSL"]
-            SNOW.name = "SNOW"
-            SNOW.to_dataset()
-            SNOW.encoding["unlimited_dims"] = "time"
-            SNOW.to_netcdf(f"{fdir}/SNOW.nc")
-            SNOW.close()
+            ds = xr.open_mfdataset([f"{fdir}/SNOW_ICE.nc", f"{fdir}/RAIN_ICE.nc"], combine="nested")
+            PRECIP_ICE = ds["SNOW_ICE"] + ds["RAIN_ICE"]
+            PRECIP_ICE.name = "PRECIP_ICE"
+            PRECIP_ICE = PRECIP_ICE.to_dataset()
+            PRECIP_ICE.encoding["unlimited_dims"] = "time"
+            PRECIP_ICE.to_netcdf(f"{fdir}/PRECIP_ICE.nc")
+            PRECIP_ICE.close()
+            ds.close()
         except:
             None
 
-    elif var=="RAIN":
+    if var=="SMB_ICE":
         try:
-            PRECT = xr.open_dataset(f"{fdir}/PRECT.nc")
-            SNOW = xr.open_dataset(f"{fdir}/SNOW.nc")
-            RAIN = PRECT["PRECT"] - SNOW["SNOW"]
-            RAIN.name = "RAIN"
-            RAIN.to_dataset()
-            RAIN.encoding["unlimited_dims"] = "time"
-            RAIN.to_netcdf(f"{fdir}/RAIN.nc")
-            RAIN.close()
+            ds = xr.open_mfdataset([f"{fdir}/SNOW_ICE.nc", f"{fdir}/QSNOFRZ_ICE.nc", f"{fdir}/QSNOMELT_ICE.nc", f"{fdir}/QICE_MELT.nc", f"{fdir}/QSOIL_ICE.nc"], combine="nested")
+            SMB_ICE = ds["SNOW_ICE"] + ds["QSNOFRZ_ICE"] - ds["QSNOMELT_ICE"] - ds["QICE_MELT"] - ds["QSOIL_ICE"]
+            SMB_ICE.name = "SMB_ICE"
+            SMB_ICE = SMB_ICE.to_dataset()
+            SMB_ICE.encoding["unlimited_dims"] = "time"
+            SMB_ICE.to_netcdf(f"{fdir}/SMB_ICE.nc")
+            SMB_ICE.close()
+            ds.close()
         except:
             None
 
-    elif var=="RADTOA":
+    if var=="RUNOFF_ICE":
         try:
-            FSNT = xr.open_dataset(f"{fdir}/FSNT.nc")
-            FLNT = xr.open_dataset(f"{fdir}/FLNT.nc")
-            RADTOA = FSNT["FSNT"] - FLNT["FLNT"]
-            RADTOA.name = "RADTOA"
-            RADTOA.to_dataset()
-            RADTOA.encoding["unlimited_dims"] = "time"
-            RADTOA.to_netcdf(f"{fdir}/RADTOA.nc")
-            RADTOA.close()
+            ds = xr.open_mfdataset([f"{fdir}/QSNOMELT_ICE.nc", f"{fdir}/QICE_MELT.nc", f"{fdir}/RAIN_ICE.nc", f"{fdir}/QSNOFRZ_ICE.nc"], combine="nested")
+            RUNOFF_ICE = ds["QSNOMELT_ICE"] + ds["QICE_MELT"] + ds["RAIN_ICE"] - ds["QSNOFRZ_ICE"]
+            RUNOFF_ICE.name = "RUNOFF_ICE"
+            RUNOFF_ICE = RUNOFF_ICE.to_dataset()
+            RUNOFF_ICE.encoding["unlimited_dims"] = "time"
+            RUNOFF_ICE.to_netcdf(f"{fdir}/RUNOFF_ICE.nc")
+            RUNOFF_ICE.close()
+            ds.close()
+        except:
+            None
+
+    if var=="FSA_ICE":
+        try:
+            ds = xr.open_mfdataset([f"{fdir}/FSDS.nc", f"{fdir}/FSR_ICE.nc"], combine="nested")
+            FSA_ICE = ds["FSDS"] - ds["FSR_ICE"]
+            FSA_ICE.name = "FSA_ICE"
+            FSA_ICE = FSA_ICE.to_dataset()
+            FSA_ICE.encoding["unlimited_dims"] = "time"
+            FSA_ICE.to_netcdf(f"{fdir}/FSA_ICE.nc")
+            FSA_ICE.close()
+            ds.close()
+        except:
+            None
+
+    if var=="FIRA_ICE":
+        try:
+            ds = xr.open_mfdataset([f"{fdir}/FLDS_ICE.nc", f"{fdir}/FIRE_ICE.nc"], combine="nested")
+            FIRA_ICE = ds["FLDS_ICE"] - ds["FIRE_ICE"]
+            FIRA_ICE.name = "FIRA_ICE"
+            FIRA_ICE = FIRA_ICE.to_dataset()
+            FIRA_ICE.encoding["unlimited_dims"] = "time"
+            FIRA_ICE.to_netcdf(f"{fdir}/FIRA_ICE.nc")
+            FIRA_ICE.close()
+            ds.close()
+        except:
+            None
+
+    if var=="MELTHEAT_ICE":
+        try:
+            ds = xr.open_mfdataset([f"{fdir}/MELT_ICE.nc"], combine="nested")
+            MELTHEAT_ICE = ds["MELT_ICE"] * 3.337e+5
+            MELTHEAT_ICE.name = "MELTHEAT_ICE"
+            MELTHEAT_ICE = MELTHEAT_ICE.to_dataset()
+            MELTHEAT_ICE.encoding["unlimited_dims"] = "time"
+            MELTHEAT_ICE.to_netcdf(f"{fdir}/MELTHEAT_ICE.nc")
+            MELTHEAT_ICE.close()
+            ds.close()
+        except:
+            None
+
+    if var=="GHF_ICE":
+        try:
+            ds = xr.open_mfdataset([f"{fdir}/MELTHEAT_ICE.nc", f"{fdir}/FSA_ICE.nc", f"{fdir}/FIRA_ICE.nc", f"{fdir}/EFLX_LH_TOT_ICE.nc", f"{fdir}/FSH_ICE.nc"], combine="nested")
+            MELTHEAT_ICE = ds["MELTHEAT_ICE"] - ds["FSA_ICE"] - ds["FIRA_ICE"] + ds["EFLX_LH_TOT_ICE"] + ds["FSH_ICE"]
+            MELTHEAT_ICE.name = "MELTHEAT_ICE"
+            MELTHEAT_ICE = MELTHEAT_ICE.to_dataset()
+            MELTHEAT_ICE.encoding["unlimited_dims"] = "time"
+            MELTHEAT_ICE.to_netcdf(f"{fdir}/MELTHEAT_ICE.nc")
+            MELTHEAT_ICE.close()
+            ds.close()
         except:
             None
 
@@ -83,7 +135,7 @@ def main():
     size = comm.Get_size()
 
     fdir = f"{config['run']['folder']}/{config['run']['name']}/lnd/hist/monavg"
-    varlist = ["ALBEDO", "PRECT", "SNOW", "RAIN", "RADTOA"]
+    varlist = ["ALBEDO_ICE", "MELT_ICE", "PRECIP_ICE", "SMB_ICE", "RUNOFF_ICE", "FSA_ICE", "FIRA_ICE", "MELTHEAT_ICE", "GHF_ICE"]
     varlist = lib.mpimods.check_varlist(varlist, size)
 
     for i in range(int(len(varlist)/size)):
