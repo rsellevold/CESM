823f2004a7535caf5a83dac3cd2c08a28f459f8f
rsellevold <raymond.sellevold@gmail.com>
2021-03-10 16:56:08 +0100

    .

--- main_proc.py
+++ main_proc.py
@@ -2,14 +2,16 @@ import yaml
 with open("config.yml","r") as f:
     config = yaml.safe_load(f)
 
-scripts = ["process/all/hist",
-           "process/all/avg_mon2ann",
-           "process/all/avg_mon2seas",
-           "process/all/timeseries",
-           "process/atm/calcadd",
-           "process/atm/trends",
-           "process/lnd/calcadd",
-           "process/lnd/trends"]
+scripts = ["process/all/hist.py <components>",
+           "process/atm/calcadd.py",
+           "process/lnd/calcadd.py",
+           "process/lnd/masks.py",
+           "process/all/avg_day2mon.py <components>",
+           "process/all/avg_mon2ann.py <components>",
+           "process/all/avg_mon2seas.py <components>",
+           "process/all/timeseries.py <components> <time>",
+           "process/all/trends.py <components> <time>",
+           "process/all/timeseries_masks.py <components> <time> <path-to-mask>"]
 
 nproc = config["machine"]["nproc"]
 
@@ -26,11 +28,11 @@ procfile.write(f"source {config['machine']['condaact']} {config['machine']['cond
 for i in range(len(scripts)):
     if config["machine"]["mpi"]:
         if scripts[i] == "process/atm/calcadd":
-            procfile.write(f"mpiexec -n 1 python {config['machine']['codepath']}/{scripts[i]}.py\n")
+            procfile.write(f"mpiexec -n 1 python {config['machine']['codepath']}/{scripts[i]}\n")
         elif scripts[i] == "process/lnd/calcadd":
-            procfile.write(f"mpiexec -n 1 python {config['machine']['codepath']}/{scripts[i]}.py\n")
+            procfile.write(f"mpiexec -n 1 python {config['machine']['codepath']}/{scripts[i]}\n")
         else:
-            procfile.write(f"mpiexec -n {config['machine']['nproc']} python {config['machine']['codepath']}/{scripts[i]}.py\n")
+            procfile.write(f"mpiexec -n {config['machine']['nproc']} python {config['machine']['codepath']}/{scripts[i]}\n")
 
     else:
         procfile.write(f"python {config['machine']['codepath']}/{scripts[i]}.py\n")
